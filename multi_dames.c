260aa1e79a3ad92cbe261ba6ebf19121cec1b8b2
Mathieu.Vilmot <mathieu.vilmot@gmial.com>
2025-10-21 14:09:32 +0200

    Same changes as you did to merge

--- multi_dames.c
+++ multi_dames.c
@@ -201,6 +201,7 @@ int jeu_initialisation(Jeu *jeu) {
     int x, y;
     for (int i = 0; i < jeu->nb_joueurs; i++)
     {
+        jeu->joueur_courant=i;
         x = -1;
         y = -1;
         
@@ -321,14 +322,14 @@ int main(){
     printf("----------\\ Plateau initial /----------\n");
     jeu_affiche(&game);
 
-
-    jeu_initialisation(&game);
-
+    
     for (int i = 0; i < game.nb_joueurs; i++)
     {
         game.joueur[i].etat = 1;
         game.joueur[i].score = 0;
     }
+    
+        jeu_initialisation(&game);
 
     game.tour = 0;
     int game_over = 0;
